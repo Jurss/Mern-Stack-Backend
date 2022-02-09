8402db65e0e9851596490369197d9fb103845803
Jurss <remi62120@hotmail.fr>
2022-02-09 18:21:23 +0100

    Add middleware for token check

--- controllers/user.js
+++ controllers/user.js
@@ -26,12 +26,12 @@ exports.login = (req, res, next) => {
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'Mot de passe non valable !' });
-                    };
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    }
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign({ userId: user._id },
-                            'RANDOM_TOKEN_SECRET', { expiresIn: '24' }
+                            'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }
                         )
                     });
                 })
