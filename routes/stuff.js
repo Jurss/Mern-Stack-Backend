829393a114c9022058c2b81cbe8c055c2b981af2
Jurss <remi62120@hotmail.fr>
2022-02-11 09:54:27 +0100

    modify put stuff routes

--- routes/stuff.js
+++ routes/stuff.js
@@ -5,7 +5,7 @@ const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-config');
 
 router.post('/', auth, multer, stuffCtrl.createThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.get('/', auth, stuffCtrl.getAllThing);
